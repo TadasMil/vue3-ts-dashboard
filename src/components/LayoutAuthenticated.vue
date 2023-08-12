f9a0ffcc2ee6fbe12362e926a557f4913cdb46da
tytadas <ttadas10@gmail.com>
2023-08-12 18:32:43 +0300

    (feat) - adjust layout to include toolbar

--- src/components/LayoutAuthenticated.vue
+++ src/components/LayoutAuthenticated.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts" name="LayoutAuthenticated">
 import SidebarNavigation from '@/components/SidebarNavigation.vue'
+import Toolbar from '@/components/Toolbar.vue'
 </script>
 
 <template>
   <div class="base-layout">
-    <SidebarNavigation />
+    <SidebarNavigation class="sidebarArea" />
+    <Toolbar class="toolbarArea" />
 
     <slot />
   </div>
@@ -14,7 +16,21 @@ import SidebarNavigation from '@/components/SidebarNavigation.vue'
 .base-layout {
   display: grid;
   grid-template-columns: auto 1fr;
+  grid-template-rows: auto 1fr;
   height: 100vh;
-  gap: 50px;
+  gap: 20px 50px;
+
+  /* Grid areas */
+  grid-template-areas:
+    'sidebar toolbar'
+    'sidebar content';
+}
+
+.sidebarArea {
+  grid-area: sidebar;
+}
+
+.toolbarArea {
+  grid-area: toolbar;
 }
 </style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts" name="Toolbar">
+import ToolbarSearch from '@/components/ToolbarSearch.vue'
+</script>
+
+<template>
+  <div class="toolbar">
+    <ToolbarSearch />
+  </div>
+</template>
+
+<style scoped>
+.toolbar {
+  width: 100%;
+  border-bottom: 1px solid #f1f1f1;
+  padding: 24px 0;
+}
+</style>
